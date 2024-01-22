b251e46d7cf54b60d704451d4449012602e5884b
Isaac Cruz <Isaac Cruz@EAGLELT>
2024-01-21 21:26:41 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,117 @@
+﻿// this is a single-line comment
+/* this is a multi-line comment
+ * 
+ * Author:  Isaac Cruz
+ * Course: COMP-003A-L01
+ * Purpose: Code for Assignment 1
+ */
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Me");
+            Console.WriteLine("1. I am 18.");
+            Console.WriteLine("2. I live with my parents.");
+            Console.WriteLine("3. I never lived in a house, only apartments.");
+            Console.WriteLine("4. I have green eyes.");
+            Console.WriteLine("5. I only have 1 brother.");
+            Console.WriteLine("6. I was born in california.");
+            Console.WriteLine("7. I am mixed race.");
+            Console.WriteLine("8. I had 2 seizures growing up.");
+            Console.WriteLine("9. I was named after my Tia.");
+            Console.WriteLine("10. My name is Isaac Nathaniel Cruz.");
+            Console.WriteLine("11. I was born in lancaster california but raised in pacoima california.");
+            Console.WriteLine("12. I was born in the year 2005.");
+            Console.WriteLine("13. I got my last name from my dad.");
+            Console.WriteLine("14. My birthday is June 15th");
+            Console.WriteLine("15. I lived half of my life in lemoore.");
+            Console.WriteLine("16. I moved to 3 different cities near LA before moving to lemoore.");
+            Console.WriteLine("17. Im 6 foot tall.");
+            Console.WriteLine("18. I am prescribed to wear glasses.");
+            Console.WriteLine("19. I like to workout.");
+            Console.WriteLine("20. I am into computers.");
+            Console.WriteLine("21. I have 12 immediate cousins.");
+            Console.WriteLine("22. I can bench 265lbs.");
+            Console.WriteLine("23. I am a gemini.");
+            Console.WriteLine("24. I have 2 tias from my dad side.");
+            Console.WriteLine("25. I have 1 tia and 2 tios on my mom side.");
+            Console.WriteLine("26. I moved to lemoore in 2014.");
+            Console.WriteLine("27. I was 9 years old when I moved here.");
+            Console.WriteLine("28. I went to engvall elementary 3rd-6th grade.");
+            Console.WriteLine("29. I went to liberty middle school from 7th-8th.");
+            Console.WriteLine("30. I went to Lemoore High from 9th-12th grade.");
+            Console.WriteLine("31. I graduated with a 3.42 gpa.");
+            Console.WriteLine("32. I am going for my IT degree and 3 certificates.");
+            Console.WriteLine("33. I used to record youtube videos.");
+            Console.WriteLine("34. I moved 3 times while living in lemoore.");
+            Console.WriteLine("35. I am family-oriented.");
+            Console.WriteLine("36. I have 10 fingers.");
+            Console.WriteLine("37. First place I traveled to was mexico when I was 1.");
+            Console.WriteLine("38. Second placed I travled far was Oregon.");
+            Console.WriteLine("39. More of my mom side of family live in lemoore.");
+            Console.WriteLine("40. I built my own desktop computer.");
+            Console.WriteLine("41. I dont have tattoos yet.");
+            Console.WriteLine("42. I went to mexico with my mom side of family.");
+            Console.WriteLine("43. I only went to 2 weddings my entire life.");
+            Console.WriteLine("44. Me and my brother are 7 and half years apart.");
+            Console.WriteLine("45. I went to disneyland for senior grad night.");
+            Console.WriteLine("46. I went to knotts berry farm twice.");
+            Console.WriteLine("47. Iam guatamalan, mexican and american.");
+            Console.WriteLine("48. I am the youngest sibling.");
+            Console.WriteLine("49. I strive to be the best version of myself.");
+            Console.WriteLine("50. I look for the best in people.");
+            Console.WriteLine("51. I am not a quitter.");
+            Console.WriteLine("52. I have a girlfriend.");
+            Console.WriteLine("53. I enjoy rap music.");
+            Console.WriteLine("54. I enjoy cumbia music.");
+            Console.WriteLine("55. I enjoy raggaeton music.");
+            Console.WriteLine("56. I am motivated by listening to music.");
+            Console.WriteLine("57. I work better in groups.");
+            Console.WriteLine("58. I enjoy going to west hills.");
+            Console.WriteLine("59. This is my second semester in college.");
+            Console.WriteLine("60. I am a size 13 in shoes.");
+            Console.WriteLine("61. I am the same size in shirts when I was in 8th grade.");
+            Console.WriteLine("62. I am a size bigger in pants/sweats than sweaters.");
+            Console.WriteLine("63. I am the only glasses wearer in my siblings.");
+            Console.WriteLine("64. I get the blindness gene from my dad side.");
+            Console.WriteLine("65. I have 10 toes.");
+            Console.WriteLine("66. I have a tan line.");
+            Console.WriteLine("67. I enjoy going on hikes.");
+            Console.WriteLine("68. I like going fishing.");
+            Console.WriteLine("69. I have a collection of playing cards.");
+            Console.WriteLine("70. I collect dutch bros stickers.");
+            Console.WriteLine("71. I have a 2.765 gpa so far.");
+            Console.WriteLine("72. I made a chess board in my senior ceramic class.");
+            Console.WriteLine("73. I enjoy learning stuff I have a passion for.");
+            Console.WriteLine("74. I work better hands on training.");
+            Console.WriteLine("75. I am a fast leraner.");
+            Console.WriteLine("76. I have 5 pairs of vans.");
+            Console.WriteLine("77. I haven't broken any bones.");
+            Console.WriteLine("78. I enjoy watching the back to the future movies.");
+            Console.WriteLine("79. I am almost fluent in spanish.");
+            Console.WriteLine("80. I am currently cutting down in weight.");
+            Console.WriteLine("81. My favorite mario character is toad.");
+            Console.WriteLine("82. My favorite pokemon is squirtle.");
+            Console.WriteLine("83. I own an iphone 14 plus.");
+            Console.WriteLine("84. I enjoy nike shoes over adidas shoes.");
+            Console.WriteLine("85. I dont really like movies as much as youtube videos.");
+            Console.WriteLine("86. My favorite show is upload on amazon prime video.");
+            Console.WriteLine("87. I have owned more android phones than iphones.");
+            Console.WriteLine("88. I go to Just Lift gym in hanford.");
+            Console.WriteLine("89. I have freckles.");
+            Console.WriteLine("90. I like buzzlight more than woody.");
+            Console.WriteLine("91. My favorite cartoon movie is the incredibles.");
+            Console.WriteLine("92. I have been wearing glasses since early 2021.");
+            Console.WriteLine("93. I like apple juice more than orange juice.");
+            Console.WriteLine("94. I like hot fritos chips.");
+            Console.WriteLine("95. My favorite food is mole tamales.");
+            Console.WriteLine("96. I am the designated aux player in the car.");
+            Console.WriteLine("97. I own my own car.");
+            Console.WriteLine("98. I haven't gotten a job yet.");
+            Console.WriteLine("99. My favorite soda is cherry coke.");
+            Console.WriteLine("100. My favorite fast food place is chic-fil-a.");
+        }
+    }
+}
